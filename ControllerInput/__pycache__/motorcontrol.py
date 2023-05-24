1f3f896985759fb88822b1177a90f30cd672912f
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-05-23 22:02:43 -0400

    added periodic function, like in FRC LOL

--- ControllerInput/__pycache__/motorcontrol.py
+++ ControllerInput/__pycache__/motorcontrol.py
@@ -6,5 +6,7 @@ class boost():
     def __init__(self, controller:XboxController, pin) -> None:
         self.motor:Motor = Motor(*pin)
         
-    
+    def periodic(self):
+        if self.motor.read["boost"]:
+            self.motor.forward(1)
     
